b471d77b4cd4baf49e686e19bd0f1f9134d58651
Michael Tran <revengex@gamerarmy.com>
2013-10-20 01:51:37 -0500

    Added a main menu + some other stuff that i'm too lazy to write here

---
+++ Assets/ExitClickEventReciever.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitClickEventReciever : MonoBehaviour {
+
+	void OnClick ()
+	{
+		Application.Quit ();
+	}
+}
---
+++ Assets/PlayClickEventReciever.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayClickEventReciever : MonoBehaviour {
+
+	void OnClick()
+	{
+		Debug.Log ("Play Clicked.");
+		//Application.LoadLevel (1);
+		Application.LoadLevel ("HauntedHouse");
+	}
+}
